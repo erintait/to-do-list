a07051b648cbb6b44949dacd182e7a3c4ff43ef9
Erin Tait <erinmtait@gmail.com>
2018-11-02 17:56:24 -0700

    Initial project setup complete with dummy data

---
+++ src/dummy_data/list.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, bread, milk, and Diet Coke',
+        _id: '001'
+    },
+    {
+        title: 'Do laundry',
+        details: 'Wash t-shirts and hoodies',
+        _id: '002'
+    },
+    {
+        title: 'Do dishes',
+        details: 'Clean forks, spoons, plates, and cups',
+        _id: '003'
+    },
+    {
+        title: 'Do homework',
+        details: 'Work on prototypes and practice problems for React',
+        _id: '004'
+    },
+    {
+        title: 'Take Maggie to groomer',
+        details: 'Make sure they shave her, trim nails, pull hair out of ears',
+        _id: '005'
+    }
+]
